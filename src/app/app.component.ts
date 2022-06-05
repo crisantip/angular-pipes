686b16347fa4cf51e6677aff97d1507378f5e36a
Christian Inapanta <cinapanta@cobiscorp.com>
2022-06-05 07:08:39 -0500

    149. FieldSet de prime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  nombre: string = 'christiAn inApAntA';
-  valor: number = 1000;
-  obj: any = {
-    nombre: 'Chris'
-  };
+  constructor(private primeNGConfig: PrimeNGConfig) {}
 
-  cambiarNombre() {
-    console.log(this.nombre);
-    console.log(this.valor);
-    console.log(this.obj);
+  ngOnInit(): void {
+    this.primeNGConfig.ripple = true
   }
 }
