a643a9c709f5aa35ec069c540b6fcc384bc7b6d9
Christian Inapanta <cinapanta@cobiscorp.com>
2022-06-06 04:46:07 -0500

    166. Implementa pipe para ordenar por nombre

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -7,23 +7,28 @@ import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 import { VuelaPipe } from './pipes/vuela.pipe';
 
 
 
 @NgModule({
   declarations: [
-    NumerosComponent,
-    NoComunesComponent,
+    // components
     BasicosComponent,
+    NoComunesComponent,
+    NumerosComponent,
     OrdenarComponent,
+
+    // pipes
     MayusculasPipe,
+    OrdenarPipe,
     VuelaPipe
   ],
   exports: [
-    NumerosComponent,
-    NoComunesComponent,
     BasicosComponent,
+    NoComunesComponent,
+    NumerosComponent,
     OrdenarComponent
   ],
   imports: [
