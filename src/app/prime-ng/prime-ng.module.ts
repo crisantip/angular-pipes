8bc5de67bfcaa0469cf1279ce6f03f1de7342949
Christian Inapanta <cinapanta@cobiscorp.com>
2022-06-06 04:17:00 -0500

    163. Agrega uso de PrimeTable y PrimeToolbar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,6 +3,8 @@ import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { MenubarModule } from "primeng/menubar";
 import { FieldsetModule } from "primeng/fieldset";
+import { TableModule } from "primeng/table";
+import { ToolbarModule } from "primeng/toolbar";
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { FieldsetModule } from "primeng/fieldset";
     ButtonModule,
     CardModule,
     FieldsetModule,
-    MenubarModule
+    MenubarModule,
+    TableModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
